29282b49dd8a61d7cd63de2e7d36dc82dc482f40
sumitkrravi <ravisumit1425@gmail.com>
2025-08-09 00:11:52 +0530

    update

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-cyber-cafe-backend.onrender.com/", // <- yahan apna backend ka Render URL daalo
+  baseURL: "https://e-cyber-cafe-backend.onrender.com", // <- yahan apna backend ka Render URL daalo
 });
 
 export default api;
